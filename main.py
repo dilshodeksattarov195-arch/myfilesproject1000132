12f4af000c6a133013bf7b8edee912cd48c3c640
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-25 20:43:42 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class emailCaveWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = emailCaveWorker()
+    print("Worker engine initialized.")
